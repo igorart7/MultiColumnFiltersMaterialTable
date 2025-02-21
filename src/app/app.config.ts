370b99b2aa7217bd88e635dacb06731c9c2e4d53
igorart7 <igorart7@gmail.com>
2025-02-21 10:18:22 -0500

    Configured material UI

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,5 +7,5 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), 
-  provideAnimationsAsync(), provideHttpClient(withFetch())]
+  provideAnimationsAsync(), provideHttpClient(withFetch()), provideAnimationsAsync()]
 };
